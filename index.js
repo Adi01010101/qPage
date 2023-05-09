9baedc78ed459b2160851740d30484f5d1916805
Adi01010101 <110529704+Adi01010101@users.noreply.github.com>
2023-05-09 20:36:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const dataDisplay = document.getElementById("data-display");
 
 retrieveDataButton.addEventListener("click", () => {
   const addressField = document.getElementById("address");
+  console.log('A0:'+addressField)
   const address = encodeURIComponent(addressField.value.trim());
-  const apiUrl =  'https://api.multiversx.com/tokens/QWT-46ac01/transfers?size=10000&receiver=${address}&status=success&order=asc';
+  console.log('A1:'+address)
+  const apiUrl =  `https://api.multiversx.com/tokens/QWT-46ac01/transfers?size=10000&receiver=${address}&status=success&order=asc`;
   console.log('AA:'+apiUrl)
 
   fetch(apiUrl)
