ca221e15dce2ba5fffc7d44192a819c8e117419c
Adi01010101 <110529704+Adi01010101@users.noreply.github.com>
2023-05-09 20:25:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ retrieveDataButton.addEventListener("click", () => {
   const addressField = document.getElementById("address");
   const address = encodeURIComponent(addressField.value.trim());
   const apiUrl =  'https://api.multiversx.com/tokens/QWT-46ac01/transfers?size=10000&receiver=${address}&status=success&order=asc';
+  console.log(apiUrl)
 
   fetch(apiUrl)
     .then(response => response.json())
