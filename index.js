ecc9bfd11ade15f11c8bad16f15c5f95f9095440
Adi01010101 <110529704+Adi01010101@users.noreply.github.com>
2023-05-09 22:51:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,41 @@ const dataDisplay = document.getElementById("data-display");
 
 retrieveDataButton.addEventListener("click", () => {
   const addressField = document.getElementById("address");
-  console.log('A0:'+addressField)
   const address = encodeURIComponent(addressField.value.trim());
-  console.log('A1:'+address)
-  const apiUrl =  `https://api.multiversx.com/tokens/QWT-46ac01/transfers?size=10000&receiver=${address}&status=success&order=asc`;
-  console.log('AA:'+apiUrl)
+  const apiUrl = `https://api.multiversx.com/tokens/QWT-46ac01/transfers?size=10000&receiver=${address}&status=success&order=asc`;
 
   fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-      dataDisplay.textContent = JSON.stringify(data);
+      const table = document.createElement("table");
+
+      // Create table header row
+      const headerRow = table.insertRow();
+      const headerCells = ["TxHash", "Sender", "Receiver", "Amount", "Timestamp"];
+      headerCells.forEach(cellText => {
+        const cell = headerRow.insertCell();
+        cell.textContent = cellText;
+      });
+
+      // Create table data rows
+      data.forEach(transaction => {
+        const row = table.insertRow();
+        const cells = [
+          transaction.txHash,
+          transaction.sender,
+          transaction.receiver,
+          transaction.value,
+          transaction.timestamp
+        ];
+        cells.forEach(cellText => {
+          const cell = row.insertCell();
+          cell.textContent = cellText;
+        });
+      });
+
+      // Display table
+      dataDisplay.innerHTML = "";
+      dataDisplay.appendChild(table);
     })
     .catch(error => {
       dataDisplay.textContent = `Error: ${error}`;
